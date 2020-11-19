753f7f234e0d738b0114ddc0b798dcbb8fc3a82d
Sanjoli Goyal <71957235+sanjoli63@users.noreply.github.com>
2020-11-19 12:25:01 +0530

    2 login screen (#27)
    Co-authored-by: Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
    Co-authored-by: Muskan4010 <72690828+Muskan4010@users.noreply.github.com>

--- lib/main_properties/routs.dart
+++ lib/main_properties/routs.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import '../Screen/NewPeriod/add_period_screen.dart';
+import '../Screen/login/create.dart';
+import '../Screen/login/login.dart';
+import '../Screen/Home/home_screen.dart';
 
 Map<String, Widget Function(BuildContext)> routeTable = {
-  // LoginScreen.routeName: (ctx) {
-  //   return LoginScreen();
-  // },
+  LoginScreen.routeName: (ctx) {
+    return LoginScreen();
+  },
+  HomeScreen.routeName: (ctx) {
+    return HomeScreen();
+  },
+  CreateScreen.routeName: (ctx) {
+    return CreateScreen();
+  },
   AddPeriodScreen.routeName: (ctx) {
     return AddPeriodScreen();
   },
