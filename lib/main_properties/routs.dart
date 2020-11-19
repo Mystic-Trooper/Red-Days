99f87c81dcc4d193a9c4c5b59d47ba12119141a2
Amit Patil <amitgpatil215@gmail.com>
2020-11-19 12:22:05 +0530

    modify screen added

--- lib/main_properties/routs.dart
+++ lib/main_properties/routs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../Screen/Profile/modify_entries_screen.dart';
 import '../Screen/NewPeriod/add_period_screen.dart';
 
 Map<String, Widget Function(BuildContext)> routeTable = {
@@ -7,5 +8,8 @@ Map<String, Widget Function(BuildContext)> routeTable = {
   // },
   AddPeriodScreen.routeName: (ctx) {
     return AddPeriodScreen();
+  },
+  ModifyScreen.routeName: (ctx) {
+    return ModifyScreen();
   },
 };
