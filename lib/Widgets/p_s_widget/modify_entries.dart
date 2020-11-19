c9527555619ae718637b033ecef73f81c2e62fbc
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2020-11-19 12:37:58 +0530

    modify screen added (#29)

--- lib/Widgets/p_s_widget/modify_entries.dart
+++ lib/Widgets/p_s_widget/modify_entries.dart
@@ -1,26 +1,35 @@
 import 'package:flutter/material.dart';
+import '../../Screen/Profile/modify_entries_screen.dart';
 
 class ModifyEntries extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.symmetric(vertical: 20),
-        height: 150,
-        width: 350,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              "Last Updated 34 days before",
-              style: TextStyle(fontSize: 18),
-            ),
-            OutlineButton(
-              child: Text("Modify"),
-              onPressed: () {},
-            )
-          ],
+      padding: EdgeInsets.symmetric(vertical: 20),
+      height: 150,
+      width: 350,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            "Last Updated 34 days before",
+            style: TextStyle(fontSize: 18),
+          ),
+          OutlineButton(
+            child: Text("View"),
+            onPressed: () {
+              Navigator.of(context).pushNamed(ModifyScreen.routeName);
+            },
+          )
+        ],
+      ),
+      decoration: BoxDecoration(
+        gradient: RadialGradient(
+          radius: 5,
+          colors: [Colors.yellow, Colors.purple, Colors.red],
         ),
-        decoration: BoxDecoration(
-            color: Colors.amber, borderRadius: BorderRadius.circular(20)));
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
   }
 }
