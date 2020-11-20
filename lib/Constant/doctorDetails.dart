8340ccf94bb2e88b4ec5430a0d7d87185c42d0e6
Amit Patil <amitgpatil215@gmail.com>
2020-11-21 00:02:38 +0530

    Added doctor model

---
+++ lib/Constant/doctorDetails.dart
@@ -0,0 +1,14 @@
+import '../model/doctor.dart';
+
+final doctorDetail = [
+  Doctor(
+    name: "Shashi Choudhari",
+    isAvailable: true,
+    location: "Noida",
+  ),
+  Doctor(
+    name: "Shashi Choudhari",
+    isAvailable: true,
+    location: "Noida",
+  ),
+];
---
+++ lib/model/doctor.dart
@@ -0,0 +1,11 @@
+class Doctor {
+  String name;
+  String location;
+  bool isAvailable;
+
+  Doctor({
+    this.name,
+    this.location,
+    this.isAvailable,
+  });
+}
