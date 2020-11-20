d9cf265e7109d325113dee8c2081d61117480fc8
sanjoli63 <sanjoligo6mar@gmail.com>
2020-11-21 02:01:37 +0530

    changing recommendation

--- lib/Screen/Recomended/recomended_screen.dart
+++ lib/Screen/Recomended/recomended_screen.dart
@@ -1,16 +1,109 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import '../../Constant/blogDetail.dart';
+import '../../model/blog.dart';
 
 class RecomendedScreen extends StatelessWidget {
+  List<Blog> bleeding(String bleedingIntensity, String pain) {
+    var i = 0;
+    List<Blog> bloglist = [];
+    print(blogDetail[i]['description']);
+    if (bleedingIntensity == 'Normal') {
+      i = 0;
+      while (i < blogDetail.length) {
+        if ((blogDetail[i]['isMenorrhagia'] == 'false') &&
+            blogDetail[i]['disease'] == 'null') {
+          print(blogDetail[i]['description']);
+          final newBlog = Blog(
+            url: blogDetail[i]['url'],
+            imageUrl: blogDetail[i]['ImageUrl'],
+            youtubeUrl: blogDetail[i]['youtubeUrl'],
+            isAgeBelow: blogDetail[i]['isAgeBelow'],
+            disease: blogDetail[i]['disease'],
+            isMenorrhagia: blogDetail[i]['isMenorrhagia'],
+            title: blogDetail[i]['title'],
+            description: blogDetail[i]['description'],
+          );
+          bloglist.add(newBlog);
+        }
+        i = i + 1;
+      }
+    } else if (bleedingIntensity == 'low') {
+      i = 0;
+      while (i < blogDetail.length) {
+        if ((blogDetail[i]['isMenorrhagia'] == 'false') &&
+            blogDetail[i]['disease'] == 'hypomenorrhea') {
+          print(blogDetail[i]['description']);
+          final newBlog = Blog(
+            url: blogDetail[i]['url'],
+            imageUrl: blogDetail[i]['ImageUrl'],
+            youtubeUrl: blogDetail[i]['youtubeUrl'],
+            isAgeBelow: blogDetail[i]['isAgeBelow'],
+            disease: blogDetail[i]['disease'],
+            isMenorrhagia: blogDetail[i]['isMenorrhagia'],
+            title: blogDetail[i]['title'],
+            description: blogDetail[i]['description'],
+          );
+          bloglist.add(newBlog);
+        }
+        i = i + 1;
+      }
+    } else if (bleedingIntensity == 'high') {
+      i = 0;
+      while (i < blogDetail.length) {
+        if ((blogDetail[i]['isMenorrhagia'] == 'true')) {
+          print(blogDetail[i]['description']);
+          final newBlog = Blog(
+            url: blogDetail[i]['url'],
+            imageUrl: blogDetail[i]['ImageUrl'],
+            youtubeUrl: blogDetail[i]['youtubeUrl'],
+            isAgeBelow: blogDetail[i]['isAgeBelow'],
+            disease: blogDetail[i]['disease'],
+            isMenorrhagia: blogDetail[i]['isMenorrhagia'],
+            title: blogDetail[i]['title'],
+            description: blogDetail[i]['description'],
+          );
+          bloglist.add(newBlog);
+        }
+        i = i + 1;
+      }
+    }
+    if (pain == 'high') {
+      i = 0;
+      while (i < blogDetail.length) {
+        if ((blogDetail[i]['disease'] == 'dysmenorrhea') ||
+            (blogDetail[i]['disease'] == 'Anemia')) {
+          print(blogDetail[i]['description']);
+          final newBlog = Blog(
+            url: blogDetail[i]['url'],
+            imageUrl: blogDetail[i]['ImageUrl'],
+            youtubeUrl: blogDetail[i]['youtubeUrl'],
+            isAgeBelow: blogDetail[i]['isAgeBelow'],
+            disease: blogDetail[i]['disease'],
+            isMenorrhagia: blogDetail[i]['isMenorrhagia'],
+            title: blogDetail[i]['title'],
+            description: blogDetail[i]['description'],
+          );
+          bloglist.add(newBlog);
+        }
+
+        i = i + 1;
+      }
+    }
+    print(bloglist[0].title);
+    return bloglist;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final bloglist = bleeding('low', 'high');
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 12, vertical: 14),
         child: ListView.builder(
-          itemCount: 8,
+          itemCount: bloglist.length,
           itemBuilder: (context, index) => Column(
             children: [
               InkWell(
@@ -31,8 +124,9 @@ class RecomendedScreen extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Center(
-                        child: SvgPicture.asset(
-                          "assets/images/choice.svg",
+                        child: Image.network(
+                          bloglist[index].imageUrl ??
+                              'https://i.pinimg.com/736x/56/58/eb/5658ebd81676b99acd753488dcadd054.jpg',
                           height: 100,
                         ),
                       ),
@@ -51,7 +145,7 @@ class RecomendedScreen extends StatelessWidget {
                         child: Column(
                           children: [
                             Text(
-                              "Title of the BLog" * 4,
+                              bloglist[index].title,
                               maxLines: 1,
                               style: TextStyle(
                                 fontSize: 18,
@@ -61,7 +155,7 @@ class RecomendedScreen extends StatelessWidget {
                             ),
                             SizedBox(height: 7),
                             Text(
-                              "This is description of the blog" * 10,
+                              bloglist[index].description,
                               maxLines: 3,
                               style: TextStyle(fontSize: 14),
                               overflow: TextOverflow.ellipsis,
