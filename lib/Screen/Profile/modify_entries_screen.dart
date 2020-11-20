3d59f7277e9412b900fb8ea803b6748adfc691f0
Amit Patil <54329870+Amitpatil215@users.noreply.github.com>
2020-11-21 00:17:47 +0530

    77 period list connect (#81)

--- lib/Screen/Profile/modify_entries_screen.dart
+++ lib/Screen/Profile/modify_entries_screen.dart
@@ -1,34 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:womenism/provider/period_provider.dart';
+import 'package:intl/intl.dart';
 
 class ModifyScreen extends StatelessWidget {
   static const routeName = 'modify-screen';
+  static const BleedingIntensity = ["Heavy", "Normal", "Low"];
+  static const PainIntensity = ["High", "Moderate", "Low"];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("List All entries"),
+    return FutureBuilder(
+      future: Provider.of<PeriodProvider>(context).getPeriodList(),
+      builder: (context, snapshot) => Scaffold(
+        appBar: AppBar(
+          title: Text("List All entries"),
+        ),
+        body: snapshot.data == null
+            ? Center(child: Text("No data Available"))
+            : ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) => Column(
+                  children: [
+                    ListTile(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      title: Text(
+                          "${DateFormat('yyyy-MM-dd EE').format(snapshot.data[index]?.from ?? DateTime.now())} to ${DateFormat('yyyy-MM-dd EE').format(snapshot.data[index]?.to ?? DateTime.now())}"),
+                      subtitle: Row(
+                        children: [
+                          Text(
+                            "Pain: " +
+                                    BleedingIntensity[
+                                        snapshot?.data[index]?.painIndex] ??
+                                "Normal",
+                          ),
+                          SizedBox(width: 10),
+                          Text(
+                            "Bleeding: " +
+                                    PainIntensity[
+                                        snapshot.data[index]?.bloodIndex] ??
+                                "Low",
+                          ),
+                        ],
+                      ),
+                    ),
+                    Divider(
+                      thickness: 1,
+                    )
+                  ],
+                ),
+              ),
       ),
-      body: ListView.builder(
-          itemBuilder: (context, index) => Column(
-                children: [
-                  ListTile(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    title: Text("28 Oct- 30 Oct 20"),
-                    subtitle: Row(
-                      children: [
-                        Text("Pain: More"),
-                        SizedBox(width: 10),
-                        Text("Bleeding: Heavy"),
-                      ],
-                    ),
-                  ),
-                  Divider(
-                    thickness: 1,
-                  )
-                ],
-              )),
     );
   }
 }
--- lib/provider/period_provider.dart
+++ lib/provider/period_provider.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:womenism/model/period.dart';
 
 class PeriodProvider with ChangeNotifier {
   void addPeriod(
@@ -24,6 +25,36 @@ class PeriodProvider with ChangeNotifier {
       });
     } catch (err) {
       print("Error in period provider");
+      print(err);
+    }
+  }
+
+  // ignore: missing_return
+  Future<List<Period>> getPeriodList() async {
+    List<Period> periodList = [];
+    try {
+      final user = FirebaseAuth.instance.currentUser;
+
+      final querrySnapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .collection("Period")
+          .get();
+      querrySnapshot.docs.forEach((element) {
+        //element.data();
+        //print(element.data()['pain']);
+        final eachPeriod = Period(
+          bloodIndex: element.data()['blood'],
+          from: DateTime.parse(element.data()['from']),
+          painIndex: element.data()['pain'],
+          to: DateTime.parse(element.data()['to']),
+        );
+        periodList.add(eachPeriod);
+      });
+
+      return periodList;
+      //print(fetchDoctor);
+    } catch (err) {
       print(err);
     }
   }
