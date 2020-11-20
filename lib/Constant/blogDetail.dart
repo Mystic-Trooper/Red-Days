09cda3df76d7e0482624af7df87328f945ab606d
unknown <19104010@mail.jiit.ac.in>
2020-11-20 16:51:56 +0530

    re-added the blog details

--- lib/Constant/blogDetail.dart
+++ lib/Constant/blogDetail.dart
@@ -3,7 +3,7 @@ var blogDetail = {
     'url': 'https://www.medicalnewstoday.com/articles/322252',
     'ImageUrl':
         'https://i.pinimg.com/736x/56/58/eb/5658ebd81676b99acd753488dcadd054.jpg',
-    'YoutubeUrl':'https://www.youtube.com/watch?v=MYZECLO6yos',
+    'YoutubeUrl': 'https://www.youtube.com/watch?v=MYZECLO6yos',
     'isAgeBelow': 19,
     'Disease': 'null',
     'isMenorrhagia': 'false',
@@ -15,7 +15,7 @@ var blogDetail = {
     'url': 'https://www.medicalnewsy.com/articles/158800',
     'ImageUrl':
         'https://image.shutterstock.com/image-vector/vector-medical-poster-anemia-symptoms-260nw-1496454248.jpg',
-    'YoutubeUrl':'https://www.youtube.com/watch?v=Vh1LZlLCfkU',
+    'YoutubeUrl': 'https://www.youtube.com/watch?v=Vh1LZlLCfkU',
     'isAgeBelow': 100,
     'isMenorrhagia': 'true',
     'disease': 'Anemia',
@@ -28,7 +28,7 @@ var blogDetail = {
         'https://www.london-gynaecology.com/medication-may-affect-menstrual-cycle/',
     'ImageUrl':
         'https://cdn.shortpixel.ai/client/q_glossy,%20ret_img/https://www.london-gynaecology.com/wp-content/uploads/medication-may-affect-menstraul-cycle-630x354.jpg',
-    'YoutubeUrl':'https://www.youtube.com/watch?v=XBEb_jnE7Uc',
+    'YoutubeUrl': 'https://www.youtube.com/watch?v=XBEb_jnE7Uc',
     'isAgeBelow': 100,
     'isMenorrhagia': 'false',
     'disease': 'null',
@@ -41,7 +41,7 @@ var blogDetail = {
         'https://my.clevelandclinic.org/health/diseases/4148-dysmenorrhea#:~:text=Dysmenorrhea%20is%20the%20medical%20term,Both%20can%20be%20treated.',
     'ImageUrl':
         'https://assets-api.kathmandupost.com/thumb.php?src=https://assets-cdn.kathmandupost.com/uploads/source/news/2019/health/shutterstock_1121719391-copy.jpg&w=900&height=601',
-    'YoutubeUrl':'https://www.youtube.com/watch?v=OSrHGH6BW-M',
+    'YoutubeUrl': 'https://www.youtube.com/watch?v=OSrHGH6BW-M',
     'isAgeBelow': 100,
     'isMenorrhagia': 'true',
     'disease': 'dysmenorrhea',
@@ -54,7 +54,7 @@ var blogDetail = {
         'https://www.mayoclinic.org/healthy-lifestyle/womens-health/in-depth/menstrual-cycle/art-20047186#:~:text=The%20menstrual%20cycle%2C%20which%20is,begins%2C%20long%20cycles%20are%20common.',
     'ImageUrl':
         'https://www.newtimes.co.rw/sites/default/files/styles/mystyle/public/main/articles/2018/06/29/menstrual-cycle.jpg',
-    'YoutubeUrl':'https://www.youtube.com/watch?v=NtTcP1MRBpQ',
+    'YoutubeUrl': 'https://www.youtube.com/watch?v=NtTcP1MRBpQ',
     'isAgeBelow': 100,
     'isMenorrhagia': 'false',
     'disease': 'null',
@@ -66,7 +66,7 @@ var blogDetail = {
     'url': 'https://www.medicalnewstoday.com/articles/178635',
     'ImageUrl':
         'https://www.pennmedicine.org/-/media/images/miscellaneous/infographics/irregular_periods_graphic_2.ashx?h=428&w=650&la=en',
-    'YoutubeUrl':'https://www.youtube.com/watch?v=xs5l8wxWCbw',
+    'YoutubeUrl': 'https://www.youtube.com/watch?v=xs5l8wxWCbw',
     'isAgeBelow': 100,
     'isMenorrhagia': 'true',
     'disease': 'irregular periods',
@@ -79,7 +79,7 @@ var blogDetail = {
         'https://bangaloremirror.indiatimes.com/opinion/you/are-there-any-gigs-to-beat-menstrual-cramps-as-workingwomen-we-are-all-ears/articleshow/54798235.cms',
     'ImageUrl':
         'https://www.mydomaine.com/thmb/C--g_EzFUpZFRgmSVoofkgK6TZ8=/1500x1000/filters:no_upscale():max_bytes(150000):strip_icc()/How-to-pamper-yourself_final-26c05f7f2f3e4d6cbb46df9146a05a4f.png',
-    'YoutubeUrl':'https://www.youtube.com/watch?v=lGUoBm0FCZQ',
+    'YoutubeUrl': 'https://www.youtube.com/watch?v=lGUoBm0FCZQ',
     'isAgeBelow': 100,
     'isMenorrhagia': 'false',
     'disease': 'null',
@@ -92,7 +92,7 @@ var blogDetail = {
         'https://www.prevention.com/health/a20499057/8-reasons-your-period-is-lighter-than-usual/',
     'ImageUrl':
         'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/pack-of-contraceptive-pills-with-instructions-royalty-free-image-950433144-1553527176.jpg?crop=0.847xw:0.847xh;0.153xw,0.00765xh&resize=768:*',
-    'YoutubeUrl':'https://www.youtube.com/watch?v=uTQ09k6eXNA',
+    'YoutubeUrl': 'https://www.youtube.com/watch?v=uTQ09k6eXNA',
     'isAgeBelow': 100,
     'isMenorrhagia': 'false',
     'disease': 'hypomenorrhea',
@@ -106,10 +106,10 @@ var blogDetail = {
         'https://www.vox.com/science-and-health/2016/11/29/13765740/is-pms-real-slate-research-cure',
     'ImageUrl':
         'https://cdn.vox-cdn.com/thumbor/yioKtT3aKFhRYcPt53tqn4kY4KQ=/0x0:1000x667/920x613/filters:focal(194x242:354x402):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/52047751/shutterstock_376181395.0.jpeg',
-    'YoutubeUrl':'https://www.youtube.com/watch?v=GzuO5tu7PHY',
+    'YoutubeUrl': 'https://www.youtube.com/watch?v=GzuO5tu7PHY',
     'isAgeBelow': 100,
     'isMenorrhagia': 'false',
-    'disease': 'Premenstrual Syndrome',
+    'disease': 'null',
     'title':
         'Of course PMS is real. The real question is why we don\'t have a cure.',
     'description':
@@ -119,7 +119,7 @@ var blogDetail = {
     'url': 'https://www.healthline.com/health/womens-health/menstrual-cup',
     'ImageUrl':
         'https://i.pinimg.com/564x/be/96/bf/be96bf9fe3ea44548bcede2e209b62b7.jpg',
-    'YoutubeUrl':'https://www.youtube.com/watch?v=JjWUPQPw_xA',
+    'YoutubeUrl': 'https://www.youtube.com/watch?v=JjWUPQPw_xA',
     'isAgeBelow': 100,
     'isMenorrhagia': 'false',
     'disease': 'null',
@@ -131,7 +131,7 @@ var blogDetail = {
     'url':
         'https://www.natracare.com/blog/9-foods-eat-period-help-cramps/#:~:text=Not%20only%20does%20dark%20chocolate,for%20when%20those%20cramps%20come!',
     'ImageUrl': 'https://www.natracare.com/wp-content/uploads/2018/10/1.jpg',
-    'YoutubeUrl':'https://www.youtube.com/watch?v=cLvsdbTC5lE',
+    'YoutubeUrl': 'https://www.youtube.com/watch?v=cLvsdbTC5lE',
     'isAgeBelow': 100,
     'isMenorrhagia': 'false',
     'disease': 'null',
@@ -143,7 +143,7 @@ var blogDetail = {
     'url':
         'https://timesofindia.indiatimes.com/life-style/health-fitness/health-news/3-ways-taking-pills-for-delaying-periods-can-affect-your-body/photostory/74102386.cms',
     'ImageUrl': 'https://static.toiimg.com/photo/74112101.cms',
-    'YoutubeUrl':'https://www.youtube.com/watch?v=43DN8Lpb1Mk',
+    'YoutubeUrl': 'https://www.youtube.com/watch?v=43DN8Lpb1Mk',
     'isAgeBelow': 100,
     'isMenorrhagia': 'false',
     'disease': 'null',
@@ -156,7 +156,7 @@ var blogDetail = {
         'https://www.helpingwomenperiod.org/why-you-should-track-your-period/',
     'ImageUrl':
         'https://www.helpingwomenperiod.org/wp-content/uploads/2020/04/pexels-photo-826349-768x512.jpg',
-    'YoutubeUrl':'https://www.youtube.com/watch?v=DvNz0niPN0Y',
+    'YoutubeUrl': 'https://www.youtube.com/watch?v=DvNz0niPN0Y',
     'isAgeBelow': 100,
     'isMenorrhagia': 'false',
     'disease': 'null',
@@ -170,7 +170,7 @@ var blogDetail = {
     'ImageUrl':
         'https://www.helpingwomenperiod.org/wp-content/uploads/2019/04/tampons-article.png',
     'YoutubeUrl': 'https://www.youtube.com/watch?v=SqnZnA3BpPY',
-    'isAgeBelow':100,
+    'isAgeBelow': 100,
     'isMenorrhagia': 'false',
     'disease': 'null',
     'title': '7 Amazing Facts About Periods That Everyone Needs To Know',
