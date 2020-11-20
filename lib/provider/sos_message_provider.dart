b1c9059416d21604f678d883e3df42ad151d00de
Amit Patil <amitgpatil215@gmail.com>
2020-11-20 21:09:05 +0530

    SOS connected

--- lib/provider/sos_message_provider.dart
+++ lib/provider/sos_message_provider.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 import 'package:sms/sms.dart';
+import 'package:womenism/provider/emergency_doctor_provider.dart';
 
-void sosDoc() {
+void sosDoc(BuildContext ctx) async {
+  print("SoS running");
+  final doctorDetails =
+      await Provider.of<EmergencyDoctorProvider>(ctx, listen: false)
+          .getEmergencyDoctor();
   SmsSender sender = new SmsSender();
   String address =
-      'sms:9410234020'; //add doctor number in place of contact added
+      //'sms:9410234020'; //add doctor number in place of contact added
+      'sms:${doctorDetails.phone}';
   sender.sendSms(new SmsMessage(address, 'HELP'));
 }
