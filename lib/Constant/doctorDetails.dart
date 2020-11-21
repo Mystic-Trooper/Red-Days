b0235b31beca37f414c36837e1e28f869af878a4
Muskan4010 <72690828+Muskan4010@users.noreply.github.com>
2020-11-21 11:13:22 +0530

    dummy doctor data (#93)

--- lib/Constant/doctorDetails.dart
+++ lib/Constant/doctorDetails.dart
@@ -2,13 +2,48 @@ import '../model/doctor.dart';
 
 final doctorDetail = [
   Doctor(
-    name: "Shashi Choudhari",
+    name: "Dr. Shashi Choudhari",
     isAvailable: true,
     location: "Noida",
   ),
   Doctor(
-    name: "Shashi Choudhari",
+    name: "Dr. Kiran Guglani",
+    isAvailable: true,
+    location: "National Highway 119",
+  ),
+  Doctor(
+    name: "Dr. Manjula Lakhanpal",
+    isAvailable: true,
+    location: "Hapur Road",
+  ),
+  Doctor(
+    name: "Dr. Anjali Gupta",
+    isAvailable: true,
+    location: "Modinagar",
+  ),
+  Doctor(
+    name: "Dr. Meenal Garg",
+    isAvailable: true,
+    location: "Delhi",
+  ),
+  Doctor(
+    name: "Dr. Kashika Singh",
+    isAvailable: true,
+    location: "Ghaziabad",
+  ),
+  Doctor(
+    name: "Dr. Astha Agarwal",
     isAvailable: true,
     location: "Noida",
+  ),
+  Doctor(
+    name: "Dr. Kavita Jain",
+    isAvailable: true,
+    location: "Noida",
+  ),
+  Doctor(
+    name: "Dr. Geeta Jain",
+    isAvailable: true,
+    location: "Meerut",
   ),
 ];
